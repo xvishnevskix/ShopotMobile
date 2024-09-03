2470e77e529e4195418b84eff31855e17bd6de58
xvishnevskix <xvishnevskix@mail.ru>
2024-09-03 12:38:10 +0400

    фикс обновления экрана пользователя

--- composeApp/src/commonMain/kotlin/org/videotrade/shopot/presentation/screens/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/org/videotrade/shopot/presentation/screens/profile/ProfileScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -73,7 +74,11 @@ class ProfileScreen(
     override fun Content() {
         val mainViewModel: MainViewModel = koinInject()
         val commonViewModel: CommonViewModel = koinInject()
-        
+
+        LaunchedEffect(Unit) {
+            mainViewModel.downloadProfile()
+        }
+
         val profile =
             if (profile !== null && anotherUser) profile else mainViewModel.profile.collectAsState(
                 initial = ProfileDTO()
