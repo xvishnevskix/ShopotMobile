798af0951b791e15dc8a6e3b119e0586cebd17af
Aslan <abubakarovdev@gmail.com>
2024-12-06 09:49:46 +0300

    Прменял пути

--- composeApp/src/commonMain/kotlin/org/videotrade/shopot/api/Path.kt
+++ composeApp/src/commonMain/kotlin/org/videotrade/shopot/api/Path.kt
@@ -8,11 +8,11 @@ object EnvironmentConfig {
 //    const val SERVER_URL: String = "https://videotradedev2.ru/api/"
 //    const val WEB_SOCKETS_URL: String = "videotradedev2.ru"
    
-//    const val SERVER_URL: String = "https://videotradedev.ru/api/"
-//    const val WEB_SOCKETS_URL: String = "videotradedev.ru"
+    const val SERVER_URL: String = "https://videotradedev.ru/api/"
+    const val WEB_SOCKETS_URL: String = "videotradedev.ru"
     
-    const val SERVER_URL: String = "https://gateway-backend.videotradedev2.ru/api/"
-    const val WEB_SOCKETS_URL: String = "message-backend.videotradedev2.ru"
+//    const val SERVER_URL: String = "https://gateway-backend.videotradedev2.ru/api/"
+//    const val WEB_SOCKETS_URL: String = "message-backend.videotradedev2.ru"
     
 }
 
